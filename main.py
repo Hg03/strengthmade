7ca2c21a8380c2d4b3088d98d5bf91a490314278
Harish Gehlot <69637720+Hg03@users.noreply.github.com>
2024-07-10 10:10:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,42 @@
 import random
 import streamlit as st
+from streamlit_player import st_player
 
 # Define the exercise lists
-upper_body_exercises = [
-    "Kettlebell Snatch",
-    "Kettlebell Zercher and Press",
-    "Kettlebell Swing",
-    "Kettlebell Halo",
-    "Kettlebell Bent-Over Row",
-]
+upper_body_exercises = {
+    "Kettlebell Snatch": "https://youtu.be/6l2Iu26oWW8?si=N_Tgsker1xrITWrg",
+    "Kettlebell Zercher and Press": "https://youtube.com/shorts/rEmwZw2blqs?si=0_0Z1AcocynfUIDp",
+    "Kettlebell Swing": "https://youtu.be/ae6nV6pFRG4?si=fpSZaDpJ8KpGaemZ",
+    "Kettlebell Halo": "https://youtu.be/Zy6bgAxPeks?si=1E8lv137u0f-CMrr",
+    "Kettlebell Bent-Over Row": "https://youtu.be/9Q9gwoXHn1o?si=VUXCZcoBgc9VuxRP",
+}
 
-lower_body_exercises = [
-    "Kettlebell Goblet Squat",
-    "Kettlebell Romanian Deadlift",
-    "Kettlebell Lunge",
-    "Kettlebell Kickstand Squat",
-    "Kettlebell Deadlift",
-]
+lower_body_exercises = {
+    "Kettlebell Goblet Squat": "https://youtu.be/7EGVKUH0K10?si=aN1EUEJj2nSG54oQ",
+    "Kettlebell Romanian Deadlift": "https://youtu.be/keiZFdUgvKQ?si=ZTbZO5AT7ZPdmnby",
+    "Kettlebell Lunge": "https://youtube.com/shorts/CEeb-FqLmwk?si=ZyabS3ky6-q8-01U",
+    "Kettlebell Kickstand Squat": "https://youtube.com/shorts/QvXbpnGayzw?si=m3XQyrtU08R-5-jv",
+    "Kettlebell Deadlift": "https://youtu.be/G8wX8wwDJsM?si=fx8JAFEnTBmUl_WM",
+}
 
-full_body_exercises = [
-    "Kettlebell Thrusters", 
-    "Kettlebell Overhead Lunges",
-    "Kettlebell Clean Squat"
-]
+full_body_exercises = {
+    "Kettlebell Thrusters": "https://youtube.com/shorts/Id0Qefh4AHc?si=xj6yu7Ud1xUsVlYj", 
+    "Kettlebell Overhead Lunges": "https://youtu.be/EDK2KTVPHU0?si=tz_IY0r4LZpwkVji",
+    "Kettlebell Clean Squat": "https://youtu.be/yXTH9_7imAQ?si=IYTtM2StXMav2ikm"
+}
 
 # Function to generate a workout
 def generate_workout(upper_count, lower_count, full_count):
     workout = []
+    demos = []
+    workout.extend(random.sample(list(upper_body_exercises.keys()), upper_count))
+    demos.extend(upper_body_exercises[workout[-1]])
+    workout.extend(random.sample(list(lower_body_exercises.keys()), lower_count))
+    demos.extend(lower_body_exercises[workout[-1]])
+    workout.extend(random.sample(list(full_body_exercises.keys()), full_count))
+    demos.extend(full_body_exercises[workout[-1]])
 
-    workout.extend(random.sample(upper_body_exercises, upper_count))
-    workout.extend(random.sample(lower_body_exercises, lower_count))
-    workout.extend(random.sample(full_body_exercises, full_count))
-
-    return workout
+    return workout, demos
 
 # Streamlit app layout
 st.title("Kettlebell Workout Generator")
@@ -44,8 +48,14 @@ lower_count = st.sidebar.slider("Number of Lower Body Exercises", 1, len(lower_b
 full_count = st.sidebar.slider("Number of Full Body Exercises", 1, len(full_body_exercises), 1)
 
 if st.button("Generate Your Savage Workout"):
-    workout = generate_workout(upper_count, lower_count, full_count)
+    workout, demos = generate_workout(upper_count, lower_count, full_count)
     st.info("Your Kettlebell Workout:")
-    for i, exercise in enumerate(workout, start=1):
-        st.write(f"Exercise {i}: {exercise}")
+    start=1
+    for exercise, demo in zip(workout, demos):
+        with st.expander(f"Exercise {i}: {exercise}"):
+            st.write("3x sets with 8-10x reps")
+            st.write("Demo to learn")
+            st_player(demo)
+            start += 1
+            
 
