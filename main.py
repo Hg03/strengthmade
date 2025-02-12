0f14c8656c05cadddb57daaaac7c43097641a960
Harish Gehlot <69637720+Hg03@users.noreply.github.com>
2025-02-12 10:39:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def put_card(workout):
     today = today_date.strftime("%B %d, %Y")
     st.write(f"Date: {today}")
     for work in workout:
-        st.write(f"Exercise => {work['excercise']} : :green[{work['sets']}] x :blue[{work['reps']}] ")
+        st.write(f"Exercise => {work['exercise']} : :green[{work['sets']}] x :blue[{work['reps']}] ")
         
 # Set page title and icon
 st.set_page_config(page_title='StrengthMade', page_icon='🛎️', layout="wide")
